eda7497e2887b3d5f9b775bc413ec6280ba62896
bHannah <borsikaaa1@gmail.com>
2024-10-02 14:18:22 +0200

    változtatás a kiiratás és a mennyiségszámítás logikájában

--- model/Model.js
+++ model/Model.js
@@ -2,11 +2,9 @@ import { motivationalQuotes } from "./adat.js"
 
 export default class Model{
     #lista = ["","","","","","","","","","","","","","","","","","","",""]
-    #menny
 
     constructor(){
         this.getLista()
-        this.#menny=0;
     }
 
     getLista(){
@@ -14,15 +12,23 @@ export default class Model{
     }
 
     feltoltLista(i){
-        let Elemek = motivationalQuotes
-        let randomIndex = Math.floor(Math.random() * Elemek.length);
+        if(this.#lista[i] === ""){
+            let Elemek = motivationalQuotes
+            let randomIndex = Math.floor(Math.random() * Elemek.length);
 
-        let kivalasztott = Elemek.splice(randomIndex, 1)[0];
-        this.#lista[i] = kivalasztott;
+            let kivalasztott = Elemek.splice(randomIndex, 1)[0];
+            this.#lista[i] = kivalasztott;
+        }
     }
 
     szamlalo(){
-        this.#menny += 1;
-        return this.#menny;
+        let menny = 0;
+        for (let i = 0; i < this.#lista.length; i++) {
+            if(this.#lista[i] != ""){
+                menny += 1
+            }
+            
+        }
+        return menny;
     }
 }
